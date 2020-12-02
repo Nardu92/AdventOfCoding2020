81630783754df8e75bf35701b84599315beaabc9
Nicola Narducci <nicola.narducci@outlook.com>
2020-12-02 19:25:40 +0100

    Solution for Day 1

---
+++ AdventOfCode/Day1/Day1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AdventOfCode.Day1
+{
+    public class Day1
+    {
+        public static int Day1_1Solution()
+        {
+            HashSet<int> foundNum = new HashSet<int>();
+            using StreamReader inputFile = new StreamReader(@"C:\Users\nicna\source\repos\AdventOfCode\AdventOfCode\Day1\Input1.1.txt");
+            string line;
+            while ((line = inputFile.ReadLine()) != null)
+            {
+                var value = Convert.ToInt32(line);
+                foundNum.Add(value);
+                var diff = 2020 - value;
+                if (foundNum.TryGetValue(diff, out int actualValue))
+                {
+                    return diff * value;
+                }
+            }
+            return -1;
+        }
+
+        public static int Day1_2Solution()
+        {
+            HashSet<int> foundNum = new HashSet<int>();
+            using StreamReader inputFile = new StreamReader(@"C:\Users\nicna\source\repos\AdventOfCode\AdventOfCode\Day1\Input1.1.txt");
+            string line;
+            while ((line = inputFile.ReadLine()) != null)
+            {
+                var value = Convert.ToInt32(line);
+                foundNum.Add(value);
+            }
+
+            foreach( int i in foundNum)
+            {
+                foreach (int j in foundNum)
+                {
+                    foreach (int k in foundNum)
+                    {
+                        if(i+j+k == 2020)
+                        {
+                            return i * j * k;
+                        }
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var v = Day1.Day1.Day1_2Solution();
+            Console.WriteLine(v);
+        }
+    }
+}
