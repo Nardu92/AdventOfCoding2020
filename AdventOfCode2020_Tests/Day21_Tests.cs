a7344a267731ed412e3bece8301b0f64e40b6c1a
Nicola Narducci <nicola.narducci@outlook.com>
2020-12-21 10:11:50 +0100

    Day 21 tests

---
+++ AdventOfCode2020_Tests/Day21_Tests.cs
@@ -0,0 +1,29 @@
+﻿using AdventOfCode;
+using System;
+using System.Collections.Generic;
+using Xunit;
+using System.Linq;
+
+namespace AdventOfCode2020_Tests
+{
+    public class Day21_Tests
+    {
+
+        [Fact]
+        private static void TestSolution1()
+        {
+            var actual = Day21.Solution1(@".\..\..\..\..\AdventOfCode\Day21\Input.txt");
+            var expected = 2203;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        private static void TestSolution2()
+        {
+            var actual = Day21.Solution2(@".\..\..\..\..\AdventOfCode\Day21\Input.txt");
+            var expected = "fqfm,kxjttzg,ldm,mnzbc,zjmdst,ndvrq,fkjmz,kjkrm";
+            Assert.Equal(expected, actual);
+        }
+        
+    }
+}
