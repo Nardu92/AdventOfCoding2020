313ba5428b14de3577c142a31f92e583e6d6d6a5
Nicola Narducci <nicola.narducci@outlook.com>
2020-12-25 10:14:58 +0100

    Day25 tests

--- AdventOfCode2020_Tests/Day25_Tests.cs
+++ AdventOfCode2020_Tests/Day25_Tests.cs
@@ -18,29 +18,12 @@ namespace AdventOfCode2020_Tests
         }
 
         [Fact]
-        private static void TestSolution1_Test()
-        {
-            var actual = Day23.Solution1("389125467");
-            var expected = "67384529";
-            Assert.Equal(expected, actual);
-        }
-        
-        [Fact]
-        private static void TestSolution2_Test()
-        {
-            var actual = Day23.Solution2("389125467");
-            var expected = 149245887792;
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
         private static void TestSolution2()
         {
-            var actual = Day23.Solution2("942387615");
+            var actual = Day25.Solution2();
             var expected = 562136730660;
             Assert.Equal(expected, actual);
         }
-
 
         [Fact]
         private static void Test_CardPrivateKey()
